6e26c6bf7ab8b16f6e6b677d5c8b9b66d1a90126
justinc667 <35312187+justinc667@users.noreply.github.com>
2021-02-10 10:43:09 -0600

    Fetch and Strip

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
+function myFunction() {
+  console.log('hello world')
+}
 function fetchfun() {
 // The code below logs the HTML code of the Google home page.
 var response = UrlFetchApp.fetch("https://u.gg/lol/champions/aatrox/counter");
-Logger.log(response.getContentText());
+const body = response.getContentText();
+// console.log('body?', typeof body);
+const data = stripfribbage(body);
+const info = JSON.parse(data)
+Logger.log(info);
+}
+
+function stripfribbage (str) {
+//  const str = 'fribbage SSR_DATA good stuff APOLLO_STATE fribbage';
+
+const parts = str.split('SSR_DATA__ = ');
+// console.log(parts[1]);
+// expected output: "fox"
+const parts2 = parts[1].split('window.__APOLLO_STATE')
+// console.log(parts2[0]);
+return parts2[0]
 }
